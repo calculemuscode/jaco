a58df310c9cb865dd77c9a4a42855608592bbdc8
Rob Simmons <rob@calculem.us>
2018-05-14 16:34:29 -0400

    Additional types and version bump

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { parseExpression, parseStatement, parseProgram } from "./parse";
+export { Syn, Position, SourceLocation, Identifier, Type, Expression, Statement, Declaration } from "./ast";
