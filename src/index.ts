92383f329379f113f527c3304f24e9bb7201626e
Rob Simmons <rob@calculem.us>
2018-09-24 14:17:52 -0400

    add more exteranl types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as ast from "./ast";
+export { typeToString } from "./print";
+export { initEmpty } from "./typecheck/globalenv";
 export { ast };
-
+export { equalTypes } from "./typecheck/types";
 export { Lang } from "./lang";
 export { parseExpression, parseStatement, parseProgram } from "./parse/index";
 
